21517703cbde6b582133e93015a7e4b44bfbd77f
Pedro Saravia <sam.caillat@gmail.com>
2024-08-29 13:43:12 +0000

    operational margin

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,9 @@
+SELECT
+    orders_id
+    ,date_date
+    ,ROUND(margin+shipping_fee-logcost-CAST(ship_cost AS FLOAT64),2) AS operational_margin
+    ,quantity
+FROM {{ref("int_orders_margin")}}
+LEFT JOIN {{ref("stg_raw__ship")}}
+USING (orders_id)
+ORDER BY orders_id DESC
