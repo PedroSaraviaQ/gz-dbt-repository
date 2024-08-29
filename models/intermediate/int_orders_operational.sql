d3163369a4894f825b41cf7fe2d97b505917dd11
Pedro Saravia <sam.caillat@gmail.com>
2024-08-29 18:12:13 +0000

    add finance sql

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -3,6 +3,12 @@ SELECT
     ,date_date
     ,ROUND(margin+shipping_fee-logcost-CAST(ship_cost AS FLOAT64),2) AS operational_margin
     ,quantity
+    ,revenue
+    ,purchase_cost
+    ,margin
+    ,shipping_fee
+    ,logcost
+    ,CAST(ship_cost AS FLOAT64) AS ship_cost
 FROM {{ref("int_orders_margin")}}
 LEFT JOIN {{ref("stg_raw__ship")}}
 USING (orders_id)
