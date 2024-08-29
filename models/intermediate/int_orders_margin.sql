df54771386c8ca751e5c94c438c377bf614d5b88
Pedro Saravia <sam.caillat@gmail.com>
2024-08-29 13:30:09 +0000

    refactor margin per order

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,26 +1,11 @@
-WITH margin_per_sale AS (
-    SELECT 
-        products_id, 
-        date_date, 
-        orders_id,
-        revenue, 
-        quantity, 
-        CAST(purchase_price AS FLOAT64), 
-        ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-        s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
-    FROM {{ref("stg_raw__sales")}} s
-    LEFT JOIN {{ref("stg_raw__product")}} p 
-    USING (products_id)
-)
-
 SELECT
     orders_id
     ,date_date
     ,ROUND(SUM(revenue),2) AS revenue
     ,CAST(SUM(quantity) AS FLOAT64) AS quantity
     ,ROUND(SUM(purchase_cost),2) AS purchase_cost
-    ,ROUND(SUM(revenue)-SUM(purchase_cost),2) AS margin
-FROM margin_per_sale
+    ,ROUND(SUM(margin),2) AS margin
+FROM {{ref("int_sales_margin")}}
 GROUP BY
     orders_id
     ,date_date
